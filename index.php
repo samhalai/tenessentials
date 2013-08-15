1a65a55d2842ef9a1a2dc3f1995aef7c8839f36d
Sameer Halai <sameer@sameerhalai.com>
2013-08-15 16:40:36 -0700

    Tweaks to content

--- index.php
+++ index.php
@@ -21,57 +21,57 @@
 		<div id="essentials">
 			<ul>
 				<li class="message">
-					When out in the backcountry, make sure you always have these ten essentials with you. You can prevent incidents and mitigate risk by planning for worse conditions than what you expect.
+					Most unfornate incidents that happen in the backcountry are preventable. Make sure you always have these ten essentials with you. You can prevent incidents and mitigate risk by planning for worse conditions than what you expect. A healthy sense of fear is a good thing.
 				</li>
 				<li>
 					<i class="icon-move"></i>
 					<h2>Navigation</h2>
-					<p>Map and Compass</p>
+					<span>A Map and Compass along with basic knowledge on how to use them. A GPS is good but remember that batteries die and electronic devices can fail.</span>
 				</li>
 				<li>
 					<i class="icon-sun"></i>
 					<h2>Sun protection</h2>
-					<p>Sunscreen and sunglasses</p>
+					<span>Sunscreen, sunglasses, a hat, long sleeves whatever is appropriate for the worst conditions you can expect</span>
 				</li>
 				<li>
 					<i class="icon-suitcase"></i>
 					<h2>Insulation</h2>
-					<p>Extra clothing</p>
+					<span>Layers and extra clothing, for both when moving or stationery and in changing weather conditions.</span>
 				</li>
 				<li>
 					<i class="icon-lightbulb"></i>
 					<h2>Illumination</h2>
-					<p>Headlamps and lanterns</p>
+					<span>Headlamps, lanterns, flashlights. Cellphone lights are not good substitutes.</span>
 				</li>
 				<li>
 					<i class="icon-medkit"></i>
 					<h2>First-aid supplies</h2>
-					<p>kits</p>
+					<span>A kit containing bandages and dressing, some basic pills etc, wraps to immobilize joints etc</span>
 				</li>
 				<li>
 					<i class="icon-fire"></i>
 					<h2>Fire</h2>
-					<p>Matches and firestarter</p>
+					<span>Water-proof matches, fire-starters, stove and fuel etc.</span>
 				</li>
 				<li>
 					<i class="icon-legal"></i>
 					<h2>Repair kit and tools</h2>
-					<p>Knives or multitools</p>
+					<span>Knives, multitools, duct tape, repair kits for things you are carrying and rely on.</span>
 				</li>
 				<li>
 					<i class="icon-food"></i>
 					<h2>Nutrition</h2>
-					<p>Extra food</p>
+					<span>Always carry more food than you think you need.</span>
 				</li>
 				<li>
 					<i class="icon-glass"></i>
 					<h2>Hydration</h2>
-					<p>Extra water</p>
+					<span>Always carry more water than you think you need and also locate places where you can replenish</span>
 				</li>
 				<li>
 					<i class="icon-umbrella"></i>
 					<h2>Emergency shelter</h2>
-					<p>A shelter</p>
+					<span>A space blanket, tent depending on the expected conditions</span>
 				</li>
 				<li class="message">
 					&copy; tenessentials.org
